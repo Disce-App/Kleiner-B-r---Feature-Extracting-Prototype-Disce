77894ffa72a6c7faf4282d279d61349e9cfbef63
Disce-App <bjarne.dudzus@student.businessschool-berlin.de>
2026-01-21 17:37:20 +0100

    Initialize grosser_baer module with imports

---
+++ grosser_baer/__init__.py
@@ -0,0 +1,4 @@
+# grosser_baer/__init__.py
+from .task_templates import get_task, get_all_tasks, get_task_choices
+from .prompts import build_feedback_prompt, get_meta_prompt, SYSTEM_PROMPT_COACH
+from .session_logger import SessionLogger
